b20d835d0c9ff3acd90a611f73a9c409f433fa59
unknown <abdumh2005@hotmail.com>
2021-03-17 19:06:30 +0400

    make it better

--- cool.js
+++ cool.js
@@ -2,6 +2,7 @@ const express = require('express');
 const socket = require('socket.io')
 const path = require('path')
 const app = express();
+let PORT = 3000
 const server = app.listen((process.env.PORT || 3000, ()=> console.log("listening port 3000")))
 const io = socket(server)
 const fs = require('fs')
