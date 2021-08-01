af26d4d7d34b0fa7ea384c85712b3b0693757d3d
unknown <abdumh2005@hotmail.com>
2021-08-01 14:49:18 +0400

    h

--- cool.js
+++ cool.js
@@ -106,9 +106,8 @@ function formatmessage(name,msg){
 }
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'home.html'));
-});
+    app.use(express.static(resolve(process.cwd(), 'client/build')))
+    app.get('*', (req, res) => {
+      res.sendFile(resolve(process.cwd(), 'client/build/cool.js'))
+    })
+  }
