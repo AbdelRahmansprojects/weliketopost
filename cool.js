1262cc53266d623c44e85686b9e8817eecec0670
unknown <abdumh2005@hotmail.com>
2021-08-01 14:40:12 +0400

    h

--- cool.js
+++ cool.js
@@ -110,5 +110,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	response.sendFile(path.join(__dirname, 'client/build', 'cool.js'));
 });
