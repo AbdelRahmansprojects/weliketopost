e18350b86d4327c00ad2476b852536908ba3ab42
unknown <abdumh2005@hotmail.com>
2021-08-04 18:49:47 +0400

    h

--- cool.js
+++ cool.js
@@ -108,7 +108,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'front_end', 'build')));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'front_end', 'build', 'cool.js'))
+      res.sendFile(path.join(__dirname, 'front_end', 'build', 'home.html'))
     });
     
   }
