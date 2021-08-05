b1dadcbb09801091407ad42f13ff876a6c0bcdd1
unknown <abdumh2005@hotmail.com>
2021-08-05 14:32:41 +0400

    h

--- cool.js
+++ cool.js
@@ -110,6 +110,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 if (process.env.NODE_ENV === 'production') {
     
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(process.cwd(), 'public/home.html'))
+      res.sendFile(path.resolve(process.cwd(), 'public/test.html'))
     })
   }
