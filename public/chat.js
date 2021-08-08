12cd7099c701d5742fc99377abf33a36aa9271cb
unknown <abdumh2005@hotmail.com>
2021-08-08 22:00:22 +0400

    j

--- public/chat.js
+++ public/chat.js
@@ -144,7 +144,8 @@ socket.on("testing", function(users){
         
         if(name == users[x].username && socket.id != users[x].id){
             window.location.replace("weliketopost.herokuapp.com")
-            socket.disconnect()           
+            socket.disconnect()
+            alert("Cant choose this name cuz its already taken")           
         }
     }
 })
